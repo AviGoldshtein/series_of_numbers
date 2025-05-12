b8f9cf1d6ba8d9e2a76419bdfc031a8fe8ca2c03
avi <a0548589558@gmail.com>
2025-05-12 15:26:18 +0300

    Add project files.

---
+++ series_of_numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace series_of_numbers
+{
+    internal class Program
+    {
+
+        static List<int> askForSeries(string[] args)
+        {
+            List<int> list = new List<int>();
+            return list;
+        }
+
+
+        static void Main(string[] args)
+        {
+            askForSeries(args);
+        }
+    }
+}
